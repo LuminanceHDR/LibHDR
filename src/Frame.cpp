39ef5af315c1f3eca9d1638df748c495d83b6cb9
Davide Anastasia <davideanastasia@users.sourceforge.net>
2011-10-26 16:19:24 +0100

    *Draft* version of LibHDR::Frame

---
+++ src/Frame.cpp
@@ -0,0 +1,157 @@
+/**
+ * This file is a part of LibHDR package.
+ * ----------------------------------------------------------------------
+ * Copyright (C) 2011 Davide Anastasia
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * ----------------------------------------------------------------------
+ *
+ * @author Davide Anastasia <davideanastasia@users.sourceforge.net>
+ *  Derived from the original work by Rafal Mantiuk <mantiuk@mpi-sb.mpg.de> for PFSTOOLS
+ *   
+ */
+
+#include <iostream>
+#include <map>
+
+#include "Frame.h"
+
+namespace LibHDR
+{
+    using namespace std;
+
+    // ctor
+    Frame::Frame( int width, int height ):
+    m_Width( width ), m_Height( height )
+    { }
+
+    // copy ctor
+    Frame::Frame(const Frame& rhs):
+        m_Width(rhs.m_Width),
+        m_Height(rhs.m_Height),
+        m_Tags(rhs.m_Tags),
+        m_Channels(rhs.m_Channels)
+    { }
+    
+    // assignment operator
+    // default behaviour
+    Frame& Frame::operator=(const Frame& rhs)
+    {
+        m_Width = rhs.m_Width;
+        m_Height = rhs.m_Height;
+        m_Tags = rhs.m_Tags;
+        m_Channels = rhs.m_Channels;
+
+        return *this;
+    }
+
+    // dtor
+    Frame::~Frame()
+    {
+        // I don't like it!!! No way I can like it!
+
+        ChannelMap::iterator it = m_Channels.begin();
+        while ( it != m_Channels.end() )
+        {
+            Channel *ch = it->second;
+
+            // Nasty trick because hashmap
+            // elements point to string that is
+            // freed by the channel
+            ChannelMap::iterator itToDelete = it;
+
+            it++;
+            m_Channels.erase( itToDelete );
+        }
+        // std::cout << "Frame::~Frame()" << std::endl;
+    }
+
+    void Frame::getXYZChannels( Channel* &X, Channel* &Y, Channel* &Z )
+    {
+        ChannelMap::iterator it;
+
+        // find X
+        it = m_Channels.find("X");
+        if ( it == m_Channels.end() )
+        {
+            X = Y = Z = NULL;
+            return;
+        }
+        X = it->second;
+
+        // find Y
+        it = m_Channels.find("Y");
+        if ( it == m_Channels.end() )
+        {
+            X = Y = Z = NULL;
+            return;
+        }
+        Y = it->second;
+
+        // find Y
+        it = m_Channels.find("Z");
+        if ( it == m_Channels.end() )
+        {
+            X = Y = Z = NULL;
+            return;
+        }
+        Z = it->second;
+    }
+
+    void Frame::createXYZChannels( Channel* &X, Channel* &Y, Channel* &Z )
+    {
+        X = createChannel("X");
+        Y = createChannel("Y");
+        Z = createChannel("Z");
+    }
+
+    Channel* Frame::getChannel( std::string name )
+    {
+        ChannelMap::iterator it = m_Channels.find(name);
+        if ( it == m_Channels.end() )
+            return NULL;
+        else
+            return it->second;
+    }
+
+    Channel* Frame::createChannel( std::string name )
+    {
+        ChannelMap::iterator it = m_Channels.find(name);
+
+        if ( it != m_Channels.end() )
+        {
+            return it->second; //m_channels[name];
+        }
+        else
+        {
+            Channel *ch = new Channel( m_Width, m_Height, name );
+            m_Channels.insert( std::pair<std::string, Channel*>(name, ch) );
+
+            return ch;
+        }
+    }
+
+    void Frame::removeChannel( Channel *ch )
+    {
+        assert( ch != NULL );
+        ChannelMap::iterator it = m_Channels.find( ch->getName() );
+        assert( it != m_Channels.end() && it->second == ch );
+
+        m_Channels.erase( it );
+        delete ch;
+    }
+
+} // namespace LibHDR
+
---
+++ src/Frame.h
@@ -0,0 +1,171 @@
+/**
+ * This file is a part of LibHDR package.
+ * ----------------------------------------------------------------------
+ * Copyright (C) 2011 Davide Anastasia
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * ----------------------------------------------------------------------
+ *
+ * @author Davide Anastasia <davideanastasia@users.sourceforge.net>
+ *  Derived from the original work by Rafal Mantiuk <mantiuk@mpi-sb.mpg.de> for PFSTOOLS
+ *   
+ */
+
+#ifndef LIBHDR_FRAME
+#define LIBHDR_FRAME
+
+#include <string>
+
+#include "DLLDefines.h"
+#include "Channel.h"
+#include "Tag.h"
+
+namespace LibHDR
+{
+    /**
+     * Interface representing a single PFS frame. Frame may contain 0
+     * or more channels (e.g. color XYZ, depth channel, alpha
+     * channnel). All the channels are of the same size. Frame can
+     * also contain additional information in tags (see getTags).
+     */
+    class LIBHDR_API Frame
+    {
+    protected:
+        int             m_Width;
+        int             m_Height;
+
+        TagContainer    m_Tags;
+        ChannelMap      m_Channels;
+
+    public:
+        Frame(int width, int height);
+        Frame(const Frame&);
+        Frame& operator=(const Frame&);
+        virtual ~Frame();
+
+        /**
+         * Gets width of the channels (in pixels).
+         */
+        int getWidth() const;
+
+        /**
+         * Gets height of the channels (in pixels).
+         */
+        int getHeight() const;
+
+        /**
+         * Gets color channels in XYZ color space. May return NULLs
+         * if such channels do not exist. Values assigned to
+         * X, Y, Z are always either all NULLs or valid pointers to
+         * channels.
+         *
+         * @param X [out] a pointer to store X channel in
+         * @param Y [out] a pointer to store Y channel in
+         * @param Z [out] a pointer to store Z channel in
+         */
+        void getXYZChannels( Channel* &X, Channel* &Y, Channel* &Z );
+
+        /**
+         * Creates color channels in XYZ color space. If such channels
+         * already exists, returns existing channels, rather than
+         * creating new ones.  Note, that nothing can be assumed about
+         * the content of each channel.
+         *
+         * @param X [out] a pointer to store X channel in
+         * @param Y [out] a pointer to store Y channel in
+         * @param Z [out] a pointer to store Z channel in
+         */
+        void createXYZChannels( Channel* &X, Channel* &Y, Channel* &Z );
+
+        /**
+         * Gets a named channel.
+         *
+         * @param name [in] name of the channel. Name must be 8 or less
+         * character long.
+         * @return channel or NULL if the channel does not exist
+         */
+        Channel* getChannel( std::string name );
+
+        /**
+         * Creates a named channel. If the channel already exists, returns
+         * existing channel.
+         *
+         * Note that new channels should be created only for the first
+         * frame. The channels should not changes for the subsequent
+         * frames of a sequence.
+         *
+         * @param name [in] name of the channel. Name must be 8 or less
+         * character long.
+         * @return existing or newly created channel
+         */
+        Channel *createChannel( std::string name );
+
+        /**
+         * Removes a channel. It is safe to remove the channel pointed by
+         * the ChannelIterator.
+         *
+         * @param channel [in] channel that should be removed.
+         */
+        void removeChannel( Channel *ch ); // ?!??!?!?!?!??!???!
+
+        /**
+         *
+         * Returns STL Map of Channels.
+         *
+         */
+        ChannelMap& getChannels();
+        const ChannelMap& getChannels() const;
+
+        /**
+         * Returns reference to TagContainer that can be used
+         * to access or modify tags associated with this Frame object.
+         */
+        TagContainer& getTags();
+        const TagContainer& getTags() const;
+    };
+
+    // Inline functions
+    inline int Frame::getWidth() const
+    {
+        return m_Width;
+    }
+
+    inline int Frame::getHeight() const
+    {
+        return m_Height;
+    }
+
+    inline ChannelMap& Frame::getChannels()
+    {
+        return m_Channels;
+    }
+
+    inline const ChannelMap& Frame::getChannels() const
+    {
+        return m_Channels;
+    }
+
+    inline TagContainer& Frame::getTags()
+    {
+        return m_Tags;
+    }
+
+    inline const TagContainer& Frame::getTags() const
+    {
+        return m_Tags;
+    }
+}
+
+#endif // LIBHDR_FRAME
