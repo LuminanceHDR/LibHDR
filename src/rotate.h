dd7b1325b3631a1fbdf7a48a2adda363c5a38395
Davide Anastasia <davideanastasia@users.sourceforge.net>
2011-11-01 16:22:40 +0000

    Rotate functionality

---
+++ src/rotate.h
@@ -0,0 +1,41 @@
+/**
+ * This file is a part of LibHDR package.
+ * ---------------------------------------------------------------------- 
+ * Copyright (C) 2011 Davide Anastasia
+ * 
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * ---------------------------------------------------------------------- 
+ * 
+ * @author Davide Anastasia <davideanastasia@users.sourceforge.net>
+ *
+ */
+
+#ifndef LIBHDR_ROTATE_H
+#define LIBHDR_ROTATE_H
+
+#include "Array2D.h"
+#include "Frame.h"
+
+namespace LibHDR
+{
+    enum RotationMode { CLOCKWISE, COUNTERCLOCKWISE };
+
+    void rotateArray2D(const Array2D& in, Array2D& out, RotationMode mode);
+
+    Frame* getRotatedFrame(const Frame& frame, RotationMode mode);
+    void rotateFrame(Frame& frame, RotationMode mode);
+}
+
+#endif 
