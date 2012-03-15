cf0bc45abf2be168ca2f494f4e49633a0f379bca
Davide Anastasia <davideanastasia@users.sourceforge.net>
2012-03-15 07:12:38 +0000

    [TIFFReader] first draft

---
+++ src/libhdr/io/tiffreader.cpp
@@ -0,0 +1,311 @@
+/*
+ * This file is a part of LibHDR package.
+ * ----------------------------------------------------------------------
+ * Copyright (C) 2012 Davide Anastasia
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * ----------------------------------------------------------------------
+ */
+
+#include "libhdr/io/tiffreader.h"
+#include "libhdr/io/framereader.aux.h"
+#include "libhdr/frame.h"
+
+#include <tiffio.h>
+#include <algorithm>
+#include <cassert>
+#include <boost/cstdint.hpp>
+#include <boost/lambda/lambda.hpp>
+#include <boost/shared_ptr.hpp>
+
+namespace LibHDR
+{
+namespace IO
+{
+using namespace std;
+
+namespace
+{
+const float DIV255 = 1.0f/255.f;
+}
+
+struct TIFFReaderParameters
+{
+    uint32_t width;
+    uint32_t height;
+
+    uint16_t photometric_type;
+    uint16_t bit_per_sample;
+    uint16_t samples_per_pixel;
+
+    bool     has_alpha;
+};
+    /*
+  uint32 width, height;
+
+  uint16 comp;                  /// compression type
+  uint16 phot;                  /// type of photometric data
+  enum {FLOATLOGLUV, FLOAT, WORD, BYTE} TypeOfData; //FLOAT is the wasting space one, FLOATLOGLUV is Greg Ward's format
+  uint16 bps;                   /// bits per sample
+  uint16 nSamples;              /// number of channels in tiff file (only 1-3 are used)
+  bool has_alpha;
+  double stonits;               /// scale factor to get nit values
+  */
+//  };
+
+class TIFFReaderImpl
+{
+public:
+    TIFFReaderImpl(TIFFReader* tiff_reader):
+        m_TIFFReader(tiff_reader),
+        m_TIFF(NULL)
+    {}
+
+    ~TIFFReaderImpl()
+    {
+        close();
+    }
+
+    void open(const std::string& filename)
+    {
+        if ( m_TIFF != NULL )
+        {
+            TIFFClose(m_TIFF);
+        }
+        m_TIFF = TIFFOpen(filename.c_str(), "r");
+        if ( !m_TIFF )
+        {
+            throw OpenException("TIFF: could not open file for writing");
+        }
+    }
+
+    void close()
+    {
+        if ( m_TIFF != NULL )
+        {
+            TIFFClose(m_TIFF);
+            m_TIFF = NULL;
+
+            assert(m_TIFF == NULL);
+        }
+    }
+
+    bool isOpen()
+    {
+        return (m_TIFF != NULL);
+    }
+
+    void readLogLuv(boost::shared_ptr<Frame> /*frame*/, const TIFFReaderParameters& /*parameters*/)
+    {
+//        // compression type
+//        uint16_t compression_type;
+//        if ( !TIFFGetField(m_TIFF, TIFFTAG_COMPRESSION, &compression_type) )
+//        {
+//            compression_type = COMPRESSION_NONE;
+//        }
+
+//        if ( (compression_type != COMPRESSION_SGILOG) && (compression_type != COMPRESSION_SGILOG24) )
+//        {
+//            throw ReadException("TIFF: only support SGILOG compressed LogLuv data");
+//        }
+//        TIFFSetField(m_TIFF, TIFFTAG_SGILOGDATAFMT, SGILOGDATAFMT_FLOAT);
+    }
+
+    void read8BitInt(boost::shared_ptr<Frame> frame, const TIFFReaderParameters& parameters)
+    {
+        uint32_t image_length;
+        TIFFGetField(m_TIFF, TIFFTAG_IMAGELENGTH, &image_length);
+
+        tdata_t buffer =  _TIFFmalloc( TIFFScanlineSize(m_TIFF) );
+
+        float* pixels = frame->data();
+        //Pixel* pixels = Frame::pixels(frame->data());
+
+        for (uint32_t row = 0; row < image_length; ++row)
+        {
+            TIFFReadScanline(m_TIFF, buffer, row);
+
+            uint8_t* inner_loop_buffer = static_cast<uint8_t*>(buffer);
+            for (uint32_t sample = 0; sample < parameters.width; ++sample)
+            {
+                pixels[0] = powf(inner_loop_buffer[sample*parameters.samples_per_pixel]*DIV255, 2.2f);          // red
+                pixels[1] = powf(inner_loop_buffer[sample*parameters.samples_per_pixel + 1]*DIV255, 2.2f);      // green
+                pixels[2] = powf(inner_loop_buffer[sample*parameters.samples_per_pixel + 2]*DIV255, 2.2f);      // blue
+                pixels[3] = 1.0f;                                                                         // alpha
+
+                pixels += 4;
+                inner_loop_buffer += parameters.samples_per_pixel;
+            }
+        }
+
+    }
+
+    void read16BitInt(boost::shared_ptr<Frame> /*frame*/, const TIFFReaderParameters& /*parameters*/)
+    {
+
+    }
+    void read32BitFloat(boost::shared_ptr<Frame> /*frame*/, const TIFFReaderParameters& /*parameters*/)
+    {
+
+    }
+
+    Frame* readFrame(const Settings& /*settings*/)
+    {
+        if (m_TIFF == NULL) throw NotOpenException("TIFF: file not open");
+
+        TIFFReaderParameters parameters;
+
+        TIFFGetField(m_TIFF, TIFFTAG_IMAGEWIDTH, &parameters.width);
+        TIFFGetField(m_TIFF, TIFFTAG_IMAGELENGTH, &parameters.height);
+
+        if ( parameters.width || parameters.height )
+        {
+            throw ReadException("TIFF: Invalid image size");
+        }
+
+        boost::shared_ptr<Frame> frame(new Frame(parameters.width, parameters.height));
+
+//        if (!TIFFGetField(tif, TIFFTAG_STONITS, &parameters.stonits))
+//        {
+//            parameters.stonits = 1.;
+//        }
+
+        // type of photometric data
+        if ( !TIFFGetFieldDefaulted(m_TIFF, TIFFTAG_PHOTOMETRIC, &parameters.photometric_type) )
+        {
+            throw ReadException("TIFF: unspecified photometric type");
+        }
+
+        uint16_t* extra_sample_types = 0;
+        uint16_t  extra_samples_per_pixel = 0;
+
+        if (TIFFGetField(m_TIFF, TIFFTAG_EXTRASAMPLES, &extra_samples_per_pixel, &extra_sample_types ) != 1)
+        {
+            extra_samples_per_pixel = 0;
+        }
+
+        TIFFGetField(m_TIFF, TIFFTAG_SAMPLESPERPIXEL, &parameters.samples_per_pixel); // should never fail!
+
+        uint16_t samples_per_pixel = parameters.samples_per_pixel - extra_samples_per_pixel;
+        parameters.has_alpha = (extra_samples_per_pixel == 1);
+
+        if ( samples_per_pixel != 3 )
+        {
+            throw ReadException("TIFF: unsupported samples per pixel for RGB");
+        }
+
+        switch (parameters.photometric_type)
+        {
+        case PHOTOMETRIC_LOGLUV:
+        {
+            // Read DATA!
+            readLogLuv(frame, parameters);
+        }
+            break;
+        case PHOTOMETRIC_RGB:
+        {
+            if ( !TIFFGetField(m_TIFF, TIFFTAG_BITSPERSAMPLE, &parameters.bit_per_sample) )
+            {
+                throw ReadException("TIFF: unsupported bits per sample for RGB");
+            }
+
+            switch (parameters.bit_per_sample)
+            {
+            case 8:
+            {
+                // Read 8 bit per sample!
+                read8BitInt(frame, parameters);
+            }
+                break;
+            case 16:
+            {
+                // Read 16 bit per sample!
+                read16BitInt(frame, parameters);
+            }
+                break;
+            case 32:
+            {
+                // Read 32 bit per sample!
+                read32BitFloat(frame, parameters);
+            }
+                break;
+            default:
+            {
+                throw ReadException("TIFF: unsupported bits per sample for RGB");
+            }
+                break;
+            }
+        }
+            break;
+        default:
+        {
+            throw ReadException("TIFF: unsupported photometric type");
+        }
+            break;
+        }
+
+         // TO CHANGE!
+        return frame.get();
+    }
+
+private:
+    TIFFReader* m_TIFFReader;
+    TIFF* m_TIFF;
+};
+
+TIFFReader::TIFFReader()
+{
+    m_TIFFReaderImpl.reset( new TIFFReaderImpl(this) );
+}
+
+TIFFReader::~TIFFReader()
+// I don't need to call "close()", because the Pimpl's destructor will do that
+{}
+
+void TIFFReader::open(const std::string& filename)
+{
+    m_TIFFReaderImpl->open(filename);
+}
+
+void TIFFReader::close()
+{
+    m_TIFFReaderImpl->close();
+}
+
+Frame* TIFFReader::readFrame(const Settings& settings)
+{
+    return m_TIFFReaderImpl->readFrame(settings);
+}
+
+bool TIFFReader::isOpen()
+{
+    return m_TIFFReaderImpl->isOpen();
+}
+
+std::vector<std::string> TIFFReader::getID()
+{
+    vector<string> id;
+
+    id.push_back("tif");
+    id.push_back("tiff");
+
+    return id;
+}
+
+REGISTER_FRAMEREADER(TIFFReader)
+
+} // namespace IO
+} // namespace LibHDR
+
