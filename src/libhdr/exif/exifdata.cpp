ff59376208db16cc4c61c7277b97e59f99fe8844
Davide Anastasia <davideanastasia@users.sourceforge.net>
2012-03-19 22:06:18 +0000

    [EXIF] new LibHDR::Exif::ExifData utility class - holds data from exif

---
+++ src/libhdr/exif/exifdata.cpp
@@ -0,0 +1,228 @@
+/*
+ * This file is a part of LibHDR package.
+ * ----------------------------------------------------------------------
+ * Copyright (C) 2012 Davide Anastasia
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * ----------------------------------------------------------------------
+ */
+
+#include "exifdata.h"
+
+#include <exiv2/exiv2.hpp>
+#include <cmath>
+#include <iostream>
+
+namespace LibHDR
+{
+namespace Exif
+{
+
+namespace
+{
+// reflected-light meter calibration constant
+const float K = 12.07488f;
+// default ISO value for camera that do not report io
+const float DEFAULT_ISO = 100.f;
+// invalid value
+const float INVALID_VALUE = -1.f;
+// invalid value
+const float INVALID_EV_VALUE = -100000.f;
+// default EVCOMP value
+const float DEFAULT_EVCOMP = 0.0f;
+
+float logBase(float value, float base)
+{
+   return (logf(value) / logf(base));
+}
+
+}
+
+ExifData::ExifData()
+{
+    reset();
+}
+
+ExifData::ExifData(const std::string& filename)
+{
+    reset();
+    fromFile(filename);
+}
+
+void ExifData::fromFile(const std::string& filename)
+{
+    reset();
+    try
+    {
+        Exiv2::Image::AutoPtr image = Exiv2::ImageFactory::open(filename);
+        image->readMetadata();
+        Exiv2::ExifData &exif_data = image->exifData();
+
+        // if data is empty
+        if (exif_data.empty()) return;
+
+        // Exiv2 iterator in read-only
+        Exiv2::ExifData::const_iterator it = exif_data.end();
+        if ((it = exif_data.findKey(Exiv2::ExifKey("Exif.Photo.ExposureTime"))) != exif_data.end())
+        {
+            m_ExposureTime = it->toFloat();
+        }
+        else if ((it = exif_data.findKey(Exiv2::ExifKey("Exif.Photo.ShutterSpeedValue"))) != exif_data.end())
+        {
+            long num = 1;
+            long div = 1;
+            float tmp = expf(logf(2.0f) * it->toFloat());
+            if (tmp > 1)
+            {
+                div = static_cast<long>(tmp + 0.5f);
+            }
+            else
+            {
+                num = static_cast<long>(1.0f/tmp + 0.5f);
+            }
+            m_ExposureTime = static_cast<float>(num)/div;
+        }
+
+
+        if ((it = exif_data.findKey(Exiv2::ExifKey("Exif.Photo.FNumber"))) != exif_data.end())
+        {
+            m_F_Number = it->toFloat();
+        }
+        else if ((it = exif_data.findKey(Exiv2::ExifKey("Exif.Photo.ApertureValue"))) != exif_data.end())
+        {
+            m_F_Number = static_cast<float>(expf(logf(2.0f) * it->toFloat() / 2.f));
+        }
+        // some cameras/lens DO print the fnum but with value 0, and this is not allowed for ev computation purposes.
+        if (m_F_Number == 0.0f)
+        {
+            m_F_Number = INVALID_VALUE;
+        }
+
+        //if iso is found use that value, otherwise assume a value of iso=100. (again, some cameras do not print iso in exif).
+        if ((it = exif_data.findKey(Exiv2::ExifKey("Exif.Photo.ISOSpeedRatings"))) == exif_data.end())
+        {
+            m_IsoSpeed = it->toFloat();
+        }
+
+        if ((it = exif_data.findKey(Exiv2::ExifKey("Exif.Photo.ExposureBiasValue"))) == exif_data.end())
+        {
+            m_EvCompensation = it->toFloat();
+        }
+    }
+    catch (Exiv2::AnyError& e)
+    {
+        return;
+    }
+}
+
+const float& ExifData::exposureTime() const
+{
+    return m_ExposureTime;
+}
+bool ExifData::isExposureTime() const
+{
+    return (m_ExposureTime != INVALID_VALUE);
+}
+void ExifData::exposureTime(float et)
+{
+    m_ExposureTime = et;
+}
+
+const float& ExifData::isoSpeed() const
+{
+    return m_IsoSpeed;
+}
+bool ExifData::isIsoSpeed() const
+{
+    return true;
+}
+void ExifData::isoSpeed(float iso)
+{
+    m_IsoSpeed = iso;
+}
+
+const float& ExifData::fNumber() const
+{
+    return m_F_Number;
+}
+bool ExifData::isFNumber() const
+{
+    return (m_F_Number != INVALID_VALUE);
+}
+void ExifData::fNumber(float fnum)
+{
+    m_F_Number = fnum;
+}
+
+float ExifData::exposureValue() const
+{
+    if ( isFNumber() && isExposureTime() )
+    {
+        return logBase((m_F_Number*m_F_Number)/m_ExposureTime, 2.0f);
+    }
+    return INVALID_EV_VALUE;
+}
+bool ExifData::isExposureValue() const
+{
+    return (exposureValue() != INVALID_EV_VALUE);
+}
+
+const float& ExifData::exposureValueCompensation() const
+{
+    return m_EvCompensation;
+}
+bool ExifData::isExposureValueCompensation() const
+{
+    return (m_EvCompensation != 0.0f);
+}
+void ExifData::exposureValueCompensation(float evcomp)
+{
+    m_EvCompensation = evcomp;
+}
+
+float ExifData::getAverageSceneLuminance() const
+{
+    if ( isIsoSpeed() && isFNumber() && isExposureTime() )
+    {
+        return ( (m_ExposureTime * m_IsoSpeed) / (m_F_Number*m_F_Number*K) );
+    }
+    return INVALID_VALUE;
+}
+
+void ExifData::reset()
+{
+    // reset internal value
+    m_ExposureTime = INVALID_VALUE;
+    m_IsoSpeed = DEFAULT_ISO;
+    m_F_Number = INVALID_VALUE;
+    m_EvCompensation = DEFAULT_EVCOMP;
+}
+
+bool ExifData::isValid()
+{
+    return ( isIsoSpeed() && isFNumber() && isExposureTime() );
+}
+
+std::ostream& operator<<(std::ostream& out, const ExifData& exifdata)
+{
+    out << "Exposure value = " << exifdata.m_ExposureTime << ", ";
+    out << "F value = " << exifdata.m_F_Number << ", ";
+    out << "ISO = " << exifdata.m_IsoSpeed;
+
+    return out;
+}
+
+}   // namespace Exif
+}   // namespace LibHDR
---
+++ src/libhdr/exif/exifdata.h
@@ -0,0 +1,109 @@
+/*
+ * This file is a part of LibHDR package.
+ * ----------------------------------------------------------------------
+ * Copyright (C) 2012 Davide Anastasia
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * ----------------------------------------------------------------------
+ */
+
+#include <string>
+#include <stdexcept>
+#include <iosfwd>
+
+namespace LibHDR
+{
+//! \namespace Contains all the operations based on EXIF data
+namespace Exif
+{
+
+//! \class ExifData
+//! \brief Holds Exif Data
+//!
+class ExifData
+{
+public:
+    //! \brief empty ctor
+    ExifData();
+
+    //! \param[in] filename Name of source file
+    ExifData(const std::string& filename);
+
+    //! \brief read exif data from file
+    //! \param[in] filename Name of source file
+    void fromFile(const std::string& filename);
+
+    const float& exposureTime() const;
+    bool isExposureTime() const;
+    void exposureTime(float et);
+
+    const float& isoSpeed() const;
+    bool isIsoSpeed() const;
+    void isoSpeed(float iso);
+
+    const float& fNumber() const;
+    bool isFNumber() const;
+    void fNumber(float fnum);
+
+    float exposureValue() const;
+    bool isExposureValue() const;
+
+    const float& exposureValueCompensation() const;
+    bool isExposureValueCompensation() const;
+    void exposureValueCompensation(float evcomp);
+
+    //! \brief This function obtains the "average scene luminance" (cd/m^2) from an image file.
+    //! "average scene luminance" is the L (aka B) value mentioned in [1]
+    //! You have to take a log2f of the returned value to get an EV value.
+    //!
+    //! We are using K=12.07488f and the exif-implied value of N=1/3.125 (see [1]).
+    //! K=12.07488f is the 1.0592f * 11.4f value in pfscalibration's pfshdrcalibrate.cpp file.
+    //! Based on [3] we can say that the value can also be 12.5 or even 14.
+    //! Another reference for APEX is [4] where N is 0.3, closer to the APEX specification of 2^(-7/4)=0.2973.
+    //!
+    //! [1] http://en.wikipedia.org/wiki/APEX_system
+    //! [2] http://en.wikipedia.org/wiki/Exposure_value
+    //! [3] http://en.wikipedia.org/wiki/Light_meter
+    //! [4] http://doug.kerr.home.att.net/pumpkin/#APEX
+    //!
+    //! This function tries first to obtain the shutter speed from either of
+    //! two exif tags (there is no standard between camera manifacturers):
+    //! ExposureTime or ShutterSpeedValue.
+    //! Same thing for f-number: it can be found in FNumber or in ApertureValue.
+    //!
+    //! F-number and shutter speed are mandatory in exif data for EV calculation, iso is not.
+    //! \note This description is copied from the original source code in
+    //! Luminance HDR http://qtpfsgui.sourceforge.net/
+    //! \param[in] filename name of the file from which we want to extract the average scene luminance
+    float getAverageSceneLuminance() const;
+
+    //! \brief reset Exif Data
+    void reset();
+
+    //! \brief returns whether enough information are available to compute additional values
+    bool isValid();
+
+    friend std::ostream& operator<<(std::ostream& out, const ExifData& exifdata);
+private:
+    float m_ExposureTime;
+    float m_IsoSpeed;
+    float m_F_Number;
+    float m_EvCompensation;
+};
+
+std::ostream& operator<<(std::ostream& out, const ExifData& exifdata);
+
+}
+}
