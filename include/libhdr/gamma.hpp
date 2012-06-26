e328e50c4fc4c0dd637b1026d6319b996b530c40
Davide Anastasia <davideanastasia@users.sourceforge.net>
2012-06-26 06:08:49 +0100

    Gamma conversion V.2

--- include/libhdr/gamma.hpp
+++ include/libhdr/gamma.hpp
@@ -20,11 +20,11 @@
  *
  */
 
-//! \file gamma.h
+//! \file gamma.hpp
 //!
 //! \brief Gamma adjustment algorithm
 //! \author Davide Anastasia <davideanastasia@users.sourceforge.net>
-//! \date 2012-04-21
+//! \date 2012-06-25
 //! \since 0.0.1
 
 #ifndef LIBHDR_GAMMA_HPP
@@ -35,52 +35,204 @@
 
 namespace libhdr
 {
-const float DEFAULT_GAMMA = 1.8f;
 
-template <typename Out>
-struct gamma_correction_channels
+/*
+
+template <typename C1, typename C2>
+struct default_gamma_converter_impl
 {
-    gamma_correction_channels(float gamma = DEFAULT_GAMMA):
-        gamma_(1.f/gamma)
-    {}
-
-    template <typename T>
-    Out operator()(const T& in1)
+    template <typename SrcP, typename DstP>
+    void operator()(const SrcP& src, DstP& dst, float gamma) const
     {
-        return Out(255.f*powf(static_cast<float>(in1)/255.f, gamma_));
-    }
+        using namespace boost::gil;
 
-private:
-    float gamma_;
+        rgb32f_pixel_t rgb32f_pixel;                // create temporary pixel
+        gray32f_pixel_t gray_pixel;
+        color_convert(src, rgb32f_pixel);           // fill temporary pixel
+        color_convert(rgb32f_pixel, gray_pixel);    // calculate luminance
+
+        get_color(gray_pixel, gray_color_t()) =
+                    std::pow(
+                        get_color(gray_pixel, gray_color_t()),
+                        gamma - 1.f
+                        );
+
+        get_color(rgb32f_pixel, red_t()) =
+                channel_multiply(
+                    get_color(rgb32f_pixel, red_t()),
+                    get_color(gray_pixel, gray_color_t())
+                    );
+        get_color(rgb32f_pixel, green_t()) =
+                channel_multiply(
+                    get_color(rgb32f_pixel, green_t()),
+                    get_color(gray_pixel, gray_color_t())
+                    );
+        get_color(rgb32f_pixel, blue_t()) =
+                    get_color(rgb32f_pixel, blue_t()),
+                    get_color(gray_pixel, gray_color_t())
+                    );
+
+        color_convert(rgb32f_pixel, dst);     // fill final pixel
+    }
 };
 
-template <typename SrcView, typename DstView>
-void gamma_correction(const SrcView& src, const DstView& dst,
-                      float gamma = DEFAULT_GAMMA)
-{
-    using namespace boost::gil;
-/*
-    gil_function_requires<ImageViewConcept<SrcView> >();
-    gil_function_requires<MutableImageViewConcept<DstView> >();
-    gil_function_requires<ColorSpacesCompatibleConcept<
-            typename color_space_type<SrcView>::type,
-            typename color_space_type<DstView>::type> >();
 */
-    typedef typename channel_type<DstView>::type dst_channel_t;
 
-    typename SrcView::iterator src_it = src.begin();
-    for (typename DstView::iterator dst_it = dst.begin();
-         dst_it != dst.end();
-         ++dst_it, ++src_it)
+template <typename C1, typename C2>
+struct default_gamma_converter_impl
+{
+    template <typename SrcP, typename DstP>
+    void operator()(const SrcP& src, DstP& dst, float gamma) const
     {
-        static_transform(*src_it, *dst_it,
-                         gamma_correction_channels<dst_channel_t>(gamma));
+        using namespace boost::gil;
+
+        rgb32f_pixel_t rgb32f_pixel;                // create temporary pixel
+        color_convert(src, rgb32f_pixel);           // fill temporary pixel
+
+        get_color(rgb32f_pixel, red_t()) =
+                std::pow(
+                    get_color(rgb32f_pixel, red_t()),
+                    gamma
+                    );
+        get_color(rgb32f_pixel, green_t()) =
+                std::pow(
+                    get_color(rgb32f_pixel, green_t()),
+                    gamma
+                    );
+        get_color(rgb32f_pixel, blue_t()) =
+                std::pow(
+                    get_color(rgb32f_pixel, blue_t()),
+                    gamma
+                    );
+
+        color_convert(rgb32f_pixel, dst);     // fill final pixel
     }
+};
+
+
+/*
+
+template <>
+struct default_gamma_converter_impl<
+        boost::gil::rgb_t,
+        boost::gil::rgb_t
+        >
+{
+    template <typename SrcP, typename DstP>
+    void operator()(const SrcP& src, DstP& dst, float gamma) const
+    {
+        using namespace boost::gil;
+
+        gray32f_pixel_t gray_pixel;
+        color_convert(src, gray_pixel);         // calculate luminance
+
+        const bits32f c_ =
+                    std::pow(
+                        get_color(gray_pixel, gray_color_t()),
+                        gamma - 1.f
+                        );            // use luminance to multiplication factor
+
+        get_color(dst, red_t()) =
+                channel_convert<typename color_element_type<DstP, red_t>::type>(
+                    static_cast<bits32f>(
+                        channel_multiply(
+                            channel_convert<bits32f>(
+                                get_color(src, red_t())
+                                ),
+                            c_)
+                        )
+                    );
+
+        get_color(dst, green_t()) =
+                channel_convert<typename color_element_type<DstP, green_t>::type>(
+                    static_cast<bits32f>(
+                        channel_multiply(
+                            channel_convert<bits32f>(
+                                get_color(src, green_t())
+                                ),
+                            c_)
+                        )
+                    );
+
+        get_color(dst, blue_t()) =
+                channel_convert<typename color_element_type<DstP, blue_t>::type>(
+                    static_cast<bits32f>(
+                        channel_multiply(
+                            channel_convert<bits32f>(
+                                get_color(src, blue_t())
+                                ),
+                            c_)
+                        )
+                    );
+    }
+};
+*/
+
+template <>
+struct default_gamma_converter_impl<
+        boost::gil::rgb_t,
+        boost::gil::rgb_t
+        >
+{
+    template <typename SrcP, typename DstP>
+    void operator()(const SrcP& src, DstP& dst, float gamma) const
+    {
+        using namespace boost::gil;
+
+        get_color(dst, red_t()) =
+                channel_convert<typename color_element_type<DstP, red_t>::type>(
+                    static_cast<bits32f>(
+                        std::pow(
+                            channel_convert<bits32f>(
+                                get_color(src, red_t())
+                                ),
+                            gamma)
+                        )
+                    );
+
+        get_color(dst, green_t()) =
+                channel_convert<typename color_element_type<DstP, green_t>::type>(
+                    static_cast<bits32f>(
+                        std::pow(
+                            channel_convert<bits32f>(
+                                get_color(src, green_t())
+                                ),
+                            gamma)
+                        )
+                    );
+
+        get_color(dst, blue_t()) =
+                channel_convert<typename color_element_type<DstP, blue_t>::type>(
+                    static_cast<bits32f>(
+                        std::pow(
+                            channel_convert<bits32f>(
+                                get_color(src, blue_t())
+                                ),
+                            gamma)
+                        )
+                    );
+    }
+};
+
+struct default_gamma_converter
+{
+    template <typename SrcP, typename DstP>
+    void operator()(const SrcP& src, DstP& dst, float gamma) const
+    {
+        using namespace boost::gil;
+
+        typedef typename color_space_type<SrcP>::type SrcColorSpace;
+        typedef typename color_space_type<DstP>::type DstColorSpace;
+        default_gamma_converter_impl<SrcColorSpace, DstColorSpace>()(src, dst, gamma);
+    }
+};
+
+template <typename SrcP, typename DstP>
+inline void gamma_convert(const SrcP& src, DstP& dst, float gamma)
+{
+    default_gamma_converter()(src, dst, gamma);
 }
 
-
-
-
-}
+} // namespace libhdr
 
 #endif // LIBHDR_GAMMA_HPP
--- test/test_gamma.cpp
+++ test/test_gamma.cpp
@@ -1,25 +1,306 @@
-#include <boost/gil/image.hpp>
-#include <boost/gil/extension/io_new/jpeg_all.hpp>
-#include <libhdr/gamma.hpp>
-#include <string>
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE gamma_convert
 
-using namespace boost::gil;
+#include <boost/test/unit_test.hpp>
+
+#define TEST_CHECK_CLOSE(a, b) \
+    BOOST_CHECK_CLOSE(a, b, 0.0005f)
+
+#include <iostream>
+#include <cmath>
+#include <boost/gil/gil_all.hpp>
+#include <boost/gil/extension/toolbox/xyz.hpp>
+#include <libhdr/gamma.hpp>
+
+using namespace boost;
 using namespace std;
 
-int main(int argc, char** argv)
+BOOST_AUTO_TEST_SUITE(gamma_convert_test_gamma_1)
+
+BOOST_AUTO_TEST_CASE(gamma_convert_test_rgb32f_rgb32f_gray)
 {
-    string in_filename = argv[1];
-    string out_filename = argv[2];
+    gil::rgb32f_pixel_t rgb32f_pixel_src(0.5, 0.5, 0.5);
+    gil::rgb32f_pixel_t rgb32f_pixel_dst;
 
-    rgb8_image_t img;
-    read_image( in_filename, img, jpeg_tag() );
+    libhdr::gamma_convert(rgb32f_pixel_src, rgb32f_pixel_dst, 1.0f);
 
-    libhdr::gamma_correction( const_view(img), view(img) );
-
-    write_view( out_filename
-              , const_view( img )
-              , image_write_info< jpeg_tag >( 95 )
-              );
-
-    return 0;
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[0]), 0.5f);
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[1]), 0.5f);
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[2]), 0.5f);
 }
+
+BOOST_AUTO_TEST_CASE(gamma_convert_test_rgb32f_rgb32f_white)
+{
+    gil::rgb32f_pixel_t rgb32f_pixel_src(1.f, 1.f, 1.f);
+    gil::rgb32f_pixel_t rgb32f_pixel_dst;
+
+    libhdr::gamma_convert(rgb32f_pixel_src, rgb32f_pixel_dst, 1.f);
+
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[0]), 1.f);
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[1]), 1.f);
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[2]), 1.f);
+}
+
+BOOST_AUTO_TEST_CASE(gamma_convert_test_rgb32f_rgb32f_red)
+{
+    gil::rgb32f_pixel_t rgb32f_pixel_src(1.f, 0.f, 0.f);
+    gil::rgb32f_pixel_t rgb32f_pixel_dst;
+
+    libhdr::gamma_convert(rgb32f_pixel_src, rgb32f_pixel_dst, 1.f);
+
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[0]), 1.f);
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[1]), 0.f);
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[2]), 0.f);
+}
+
+BOOST_AUTO_TEST_CASE(gamma_convert_test_rgb32f_rgb32f_green)
+{
+    gil::rgb32f_pixel_t rgb32f_pixel_src(0.f, 1.f, 0.f);
+    gil::rgb32f_pixel_t rgb32f_pixel_dst;
+
+    libhdr::gamma_convert(rgb32f_pixel_src, rgb32f_pixel_dst, 1.f);
+
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[0]), 0.f);
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[1]), 1.f);
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[2]), 0.f);
+}
+
+BOOST_AUTO_TEST_CASE(gamma_convert_test_rgb32f_rgb32f_blue)
+{
+    gil::rgb32f_pixel_t rgb32f_pixel_src(0.f, 0.f, 1.f);
+    gil::rgb32f_pixel_t rgb32f_pixel_dst;
+
+    libhdr::gamma_convert(rgb32f_pixel_src, rgb32f_pixel_dst, 1.f);
+
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[0]), 0.f);
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[1]), 0.f);
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[2]), 1.f);
+}
+
+BOOST_AUTO_TEST_CASE(gamma_convert_test_rgb32f_rgb32f_black)
+{
+    gil::rgb32f_pixel_t rgb32f_pixel_src(0.f, 0.f, 0.f);
+    gil::rgb32f_pixel_t rgb32f_pixel_dst;
+
+    libhdr::gamma_convert(rgb32f_pixel_src, rgb32f_pixel_dst, 1.f);
+
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[0]), 0.f);
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[1]), 0.f);
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[2]), 0.f);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(gamma_convert_test_gamma_22)
+
+BOOST_AUTO_TEST_CASE(gamma_convert_test_rgb32f_rgb32f_gray)
+{
+    gil::rgb32f_pixel_t rgb32f_pixel_src(0.5, 0.5, 0.5);
+    gil::rgb32f_pixel_t rgb32f_pixel_dst;
+
+    libhdr::gamma_convert(rgb32f_pixel_src, rgb32f_pixel_dst, 2.2f);
+
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[0]),
+                     pow(0.5f, 2.2f));
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[1]),
+                     pow(0.5f, 2.2f));
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[2]),
+                     pow(0.5f, 2.2f));
+}
+
+BOOST_AUTO_TEST_CASE(gamma_convert_test_rgb32f_rgb32f_white)
+{
+    gil::rgb32f_pixel_t rgb32f_pixel_src(1.f, 1.f, 1.f);
+    gil::rgb32f_pixel_t rgb32f_pixel_dst;
+
+    libhdr::gamma_convert(rgb32f_pixel_src, rgb32f_pixel_dst, 2.2f);
+
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[0]),
+                     pow(1.f, 2.2f));
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[1]),
+                     pow(1.f, 2.2f));
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[2]),
+                     pow(1.f, 2.2f));
+}
+
+BOOST_AUTO_TEST_CASE(gamma_convert_test_rgb32f_rgb32f_red)
+{
+    gil::rgb32f_pixel_t rgb32f_pixel_src(1.f, 0.f, 0.f);
+    gil::rgb32f_pixel_t rgb32f_pixel_dst;
+
+    libhdr::gamma_convert(rgb32f_pixel_src, rgb32f_pixel_dst, 2.2f);
+
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[0]),
+                     pow(1.f, 2.2f));
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[1]),
+                     pow(0.f, 2.2f));
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[2]),
+                     pow(0.f, 2.2f));
+}
+
+BOOST_AUTO_TEST_CASE(gamma_convert_test_rgb32f_rgb32f_green)
+{
+    gil::rgb32f_pixel_t rgb32f_pixel_src(0.f, 1.f, 0.f);
+    gil::rgb32f_pixel_t rgb32f_pixel_dst;
+
+    libhdr::gamma_convert(rgb32f_pixel_src, rgb32f_pixel_dst, 2.2f);
+
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[0]),
+                     pow(0.f, 2.2f));
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[1]),
+                     pow(1.f, 2.2f));
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[2]),
+                     pow(0.f, 2.2f));
+}
+
+BOOST_AUTO_TEST_CASE(gamma_convert_test_rgb32f_rgb32f_blue)
+{
+    gil::rgb32f_pixel_t rgb32f_pixel_src(0.f, 0.f, 1.f);
+    gil::rgb32f_pixel_t rgb32f_pixel_dst;
+
+    libhdr::gamma_convert(rgb32f_pixel_src, rgb32f_pixel_dst, 2.2f);
+
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[0]),
+                     pow(0.f, 2.2f));
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[1]),
+                     pow(0.f, 2.2f));
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[2]),
+                     pow(1.f, 2.2f));
+}
+
+BOOST_AUTO_TEST_CASE(gamma_convert_test_rgb32f_rgb32f_black)
+{
+    gil::rgb32f_pixel_t rgb32f_pixel_src(0.f, 0.f, 0.f);
+    gil::rgb32f_pixel_t rgb32f_pixel_dst;
+
+    libhdr::gamma_convert(rgb32f_pixel_src, rgb32f_pixel_dst, 2.2f);
+
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[0]),
+                     pow(0.f, 2.2f));
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[1]),
+                     pow(0.f, 2.2f));
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[2]),
+                     pow(0.f, 2.2f));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(gamma_convert_test_gamma_mixed)
+
+BOOST_AUTO_TEST_CASE(gamma_convert_test_rgb8u_rgb8u_1)
+{
+    const float GaMmA = 1.8f;
+
+    gil::rgb8_pixel_t rgb8_pixel_src(128, 127, 126);
+    gil::rgb8_pixel_t rgb8_pixel_dst;
+
+    libhdr::gamma_convert(rgb8_pixel_src, rgb8_pixel_dst, GaMmA);
+
+    BOOST_CHECK_EQUAL(rgb8_pixel_dst[0],
+                      static_cast<boost::gil::bits8>(
+                          255.f*std::pow(
+                              static_cast<float>(rgb8_pixel_src[0])/255.f, GaMmA) + 0.5f)
+                      );
+    BOOST_CHECK_EQUAL(rgb8_pixel_dst[1],
+                      static_cast<boost::gil::bits8>(
+                          255.f*std::pow(
+                              static_cast<float>(rgb8_pixel_src[1])/255.f, GaMmA) + 0.5f)
+                      );
+    BOOST_CHECK_EQUAL(rgb8_pixel_dst[2],
+                      static_cast<boost::gil::bits8>(
+                          255.f*std::pow(
+                              static_cast<float>(rgb8_pixel_src[2])/255.f, GaMmA) + 0.5f)
+                      );
+}
+
+BOOST_AUTO_TEST_CASE(gamma_convert_test_rgb8u_rgb8u_2)
+{
+    const float GaMmA = 1.4f;
+
+    gil::rgb8_pixel_t rgb8_pixel_src(43, 21, 210);
+    gil::rgb8_pixel_t rgb8_pixel_dst;
+
+    libhdr::gamma_convert(rgb8_pixel_src, rgb8_pixel_dst, GaMmA);
+
+    BOOST_CHECK_EQUAL(rgb8_pixel_dst[0],
+                      static_cast<boost::gil::bits8>(
+                          255.f*std::pow(
+                              static_cast<float>(rgb8_pixel_src[0])/255.f, GaMmA) + 0.5f)
+                      );
+    BOOST_CHECK_EQUAL(rgb8_pixel_dst[1],
+                      static_cast<boost::gil::bits8>(
+                          255.f*std::pow(
+                              static_cast<float>(rgb8_pixel_src[1])/255.f, GaMmA) + 0.5f)
+                      );
+    BOOST_CHECK_EQUAL(rgb8_pixel_dst[2],
+                      static_cast<boost::gil::bits8>(
+                          255.f*std::pow(
+                              static_cast<float>(rgb8_pixel_src[2])/255.f, GaMmA) + 0.5f)
+                      );
+}
+
+
+
+BOOST_AUTO_TEST_CASE(gamma_convert_test_rgb32f_rgb32f_random_1)
+{
+    gil::rgb32f_pixel_t rgb32f_pixel_src(0.23f, 0.76f, 0.45f);
+    gil::rgb32f_pixel_t rgb32f_pixel_dst;
+
+    libhdr::gamma_convert(rgb32f_pixel_src, rgb32f_pixel_dst, 2.2f);
+
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[0]),
+                     pow(rgb32f_pixel_src[0], 2.2f));
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[1]),
+                     pow(rgb32f_pixel_src[1], 2.2f));
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[2]),
+                     pow(rgb32f_pixel_src[2], 2.2f));
+}
+
+
+BOOST_AUTO_TEST_CASE(gamma_convert_test_rgb32f_rgb32f_random_2)
+{
+    gil::rgb32f_pixel_t rgb32f_pixel_src(0.23f, 0.76f, 0.45f);
+    gil::rgb32f_pixel_t rgb32f_pixel_dst;
+
+    libhdr::gamma_convert(rgb32f_pixel_src, rgb32f_pixel_dst, 2.6f);
+
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[0]),
+                     pow(rgb32f_pixel_src[0], 2.6f));
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[1]),
+                     pow(rgb32f_pixel_src[1], 2.6f));
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[2]),
+                     pow(rgb32f_pixel_src[2], 2.6f));
+}
+
+BOOST_AUTO_TEST_CASE(gamma_convert_test_xyz32f_rgb32f)
+{
+    gil::xyz32f_pixel_t xyz32f_pixel_src(.2f, .1f, 0.7f);
+    gil::rgb32f_pixel_t rgb32f_pixel_dst;
+    gil::xyz32f_pixel_t xyz32f_pixel_dst;
+
+    libhdr::gamma_convert(xyz32f_pixel_src, rgb32f_pixel_dst, 1.f);
+
+    gil::color_convert(rgb32f_pixel_dst, xyz32f_pixel_dst);
+
+    TEST_CHECK_CLOSE(static_cast<float>(xyz32f_pixel_dst[0]),
+                     static_cast<float>(xyz32f_pixel_src[0]));
+    TEST_CHECK_CLOSE(static_cast<float>(xyz32f_pixel_dst[1]),
+                     static_cast<float>(xyz32f_pixel_src[1]));
+    TEST_CHECK_CLOSE(static_cast<float>(xyz32f_pixel_dst[2]),
+                     static_cast<float>(xyz32f_pixel_src[2]));
+
+    gil::rgb32f_pixel_t rgb32f_pixel_dst2;
+    gil::rgb32f_pixel_t rgb32f_pixel_dst3;
+
+    gil::color_convert(xyz32f_pixel_src, rgb32f_pixel_dst2);
+    libhdr::gamma_convert(rgb32f_pixel_dst2, rgb32f_pixel_dst3, 1.f);
+
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[0]),
+                     static_cast<float>(rgb32f_pixel_dst3[0]));
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[1]),
+                     static_cast<float>(rgb32f_pixel_dst3[1]));
+    TEST_CHECK_CLOSE(static_cast<float>(rgb32f_pixel_dst[2]),
+                     static_cast<float>(rgb32f_pixel_dst3[2]));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
